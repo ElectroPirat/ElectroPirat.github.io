f97c6b8f9c5933e5928c4d8899df76a3c9ecf1d4
ElectroPirat <60323922+ElectroPirat@users.noreply.github.com>
2020-09-06 11:57:35 +0200

    update code

--- kb/main.js
+++ kb/main.js
@@ -18,10 +18,27 @@ var _100zl = document.getElementById("100zl").value;
 var _200zl = document.getElementById("200zl").value;
 var _500zl = document.getElementById("500zl").value;
 
+    //zdobywanie objektu wynik
+    var _1w = document.getElementById("1w");
+    var _2w = document.getElementById("2w");
+    var _3w = document.getElementById("3w");
+    var _4w = document.getElementById("4w");
+    var _5w = document.getElementById("5w");
+    var _6w = document.getElementById("6w");
+    var _7w = document.getElementById("7w");
+    var _8w = document.getElementById("8w");
+    var _9w = document.getElementById("9w");
+    var _10w = document.getElementById("10w");
+    var _11w = document.getElementById("11w");
+    var _12w = document.getElementById("12w");
+    var _13w = document.getElementById("13w");
+    var _14w = document.getElementById("14w");
+    var _15w = document.getElementById("15w");
+
 //inne takie
 var wynikbox =  document.getElementById("wynikbox");
 
-wynikbox.innerText = _1gr * 0.01 +
+var wynikvar = _1gr * 0.01 +
 _2gr * 0.02 +
 _5gr * 0.05 +
 _10gr * 0.1 +
@@ -37,7 +54,39 @@ _100zl * 100 +
 _200zl * 200 +
 _500zl * 500 ;
 
+wynikbox.innerText = wynikvar.toFixed(2);
 
+var __1w = _1gr * 0.01;
+var __2w = _2gr * 0.02;
+var __3w = _5gr * 0.05;
+var __4w = _10gr * 0.1;
+var __5w = _20gr * 0.2;
+var __6w = _50gr * 0.5;
+var __7w = _1zl * 1;
+var __8w = _2zl * 2;
+var __9w = _5zl * 5;
+var __10w = _10zl * 10;
+var __11w = _20zl * 20;
+var __12w = _50zl * 50;
+var __13w = _100zl * 100;
+var __14w = _200zl * 200;
+var __15w = _500zl * 500;
+
+_1w.innerText = __1w.toFixed(2);
+_2w.innerText = __2w.toFixed(2);
+_3w.innerText = __3w.toFixed(2);
+_4w.innerText = __4w.toFixed(2);
+_5w.innerText = __5w.toFixed(2);
+_6w.innerText = __6w.toFixed(2);
+_7w.innerText = __7w.toFixed(2);
+_8w.innerText = __8w.toFixed(2);
+_9w.innerText = __9w.toFixed(2);
+_10w.innerText = __10w.toFixed(2);
+_11w.innerText = __11w.toFixed(2);
+_12w.innerText = __12w.toFixed(2);
+_13w.innerText = __13w.toFixed(2);
+_14w.innerText = __14w.toFixed(2);
+_15w.innerText = __15w.toFixed(2);
 
 console.log("oblicz koniec");
 }
