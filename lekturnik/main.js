72f0942fa71bfeef129b38a6d82e02e225ed678b
ElectroPirat <60323922+ElectroPirat@users.noreply.github.com>
2020-11-20 21:47:54 +0100

    new sub[page

---
+++ lekturnik/main.js
@@ -0,0 +1,10 @@
+function init () {
+//document.getElementById("objekty").innerText = `elo`
+}
+
+function closePopup () {
+    var cpop = document.getElementById("popup");
+    var cov = document.getElementById("overlay");
+    cpop.style.display = "none";
+    cov.style.display = "none";
+}
